b8445dda0a06b3cc94dd2dbd4f07cb7ad67fd222
Mario Oliveira <mario.oliveirag12@gmail>
2018-12-13 21:10:32 +0000

    conteudo do curso

---
+++ alurapic/src/components/shared/botao/Botao.vue
@@ -0,0 +1,61 @@
+<template>
+    <button @click="disparaAcao()" class="botao" :class="estiloDoBotao" :type="tipo"> {{ rotulo }} </button>
+
+</template>
+
+<script>
+export default {
+
+    props: ['tipo', 'rotulo', 'confirmacao', 'estilo'],
+    
+   methods: {
+
+       disparaAcao() {
+           console.log(typeof(this.confirmacao));
+           if(this.confirmacao) {
+               if(confirm('Confirma operacao?')) {
+                    this.$emit('botaoAtivado');
+                }
+                return;
+           }
+           this.$emit('botaoAtivado');
+       }
+   },
+
+   computed: {
+
+       estiloDoBotao() {
+
+           // se o valor é padrão ou não passou nada para estilo
+           if(this.estilo == 'padrao' || !this.estilo) return 'botao-padrao';
+
+           if(this.estilo == 'perigo') return 'botao-perigo';
+       }
+   }
+}
+
+
+
+</script>
+
+
+<style>
+
+.botao{
+    display: inline-block;
+    padding: 10px;
+    border-radius: 3px;
+    margin: 10px;
+    font-size: 1.2em;
+}
+.botao-perigo{
+    background: firebrick;
+    color: white;
+}
+
+.botao-padrao {
+    background: darkcyan;
+    color: white;
+}
+
+</style>
